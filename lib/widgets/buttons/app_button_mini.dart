623a182f503c9125662abc5e71c3a1623e33b3f8
juanpga3314 <juanpga3314@gmail.com>
2023-07-28 19:17:07 -0500

    changes

--- lib/widgets/buttons/app_button_mini.dart
+++ lib/widgets/buttons/app_button_mini.dart
@@ -2,27 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:presentation/presentation.dart';
 
 class AppButtonMini extends StatelessWidget {
-  final void Function()? onPressed;
+  final Function? onPressed;
   final String text;
   final Color? primary;
+  final TextAlign? textAlign;
+  final VisualDensity? visualDensity;
   const AppButtonMini({
     Key? key,
     required this.onPressed,
     required this.text,
     this.primary,
+    this.textAlign, this.visualDensity,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-        backgroundColor: primary ?? AppTheme.colors.primaryColor,
-        elevation: 0,
-        shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
-      ),
-      onPressed: onPressed,
+          backgroundColor: primary ?? AppTheme.colors.primaryColor,
+          visualDensity: visualDensity,
+          elevation: 0,
+          shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+      onPressed: onPressed == null ? null : () => onPressed?.call(),
       child: Text(
         text,
+        textAlign: textAlign,
         style: AppTheme.textStyles.white14F500,
       ),
     );
